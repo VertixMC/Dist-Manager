f612bfaaa84ee3d4423a46abb8416717f56f5cb3
Facto <33882218+Sniped@users.noreply.github.com>
2020-07-20 00:33:51 -0400

    put jenkinsClient in app.ts

--- src/Jenkins.ts
+++
@@ -1,9 +0,0 @@
-import jenkins from 'jenkins';
-import { bases } from './util/bases';
-
-export const jenkinsClient = jenkins(
-    {
-        baseUrl: `https://${process.env.JENKINS_USER}:${process.env.JENKINS_PASSWORD}@${bases.JENKINS_URL}`,
-        promisify: true
-    }
-)
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import { config } from 'dotenv';
 import bootstrap from './ServerBootstrap';
+import jenkins from 'jenkins';
+import { bases } from './util/bases';
 
 config();
+
+export const jenkinsClient = jenkins(
+    {
+        baseUrl: `https://${process.env.JENKINS_USER}:${process.env.JENKINS_PASSWORD}@${bases.JENKINS_URL}`,
+        promisify: true
+    }
+);
 
 (async() => {
 
