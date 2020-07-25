92e38b8fc5f19075e01ad2f70c21c1b35b0a012d
Facto <33882218+Sniped@users.noreply.github.com>
2020-07-25 01:19:34 -0400

    implement auth in main file

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,11 @@ import DockerClient from './structure/docker/DockerClient';
 
 config();
 
+export const auth = process.env.AUTH_TOKEN || '';
+
 export const jenkinsClient = jenkins(
     {
-        baseUrl: `https://${process.env.JENKINS_USER}:${process.env.JENKINS_PASSWORD}@${bases.JENKINS_URL}`,
+        baseUrl: `https://${process.env.JENKINS_USER || ''}:${process.env.JENKINS_PASSWORD || ''}@${bases.JENKINS_URL}`,
         promisify: true
     }
 );
