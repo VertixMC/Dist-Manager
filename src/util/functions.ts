86b121ccca63203691ce5584548de1e6973c18b0
Facto <33882218+Sniped@users.noreply.github.com>
2020-07-25 20:18:06 -0400

    manipulate string at readableArtifacts function

--- src/util/functions.ts
+++ src/util/functions.ts
@@ -5,7 +5,7 @@ export const readableArtifacts = (build: Build) => {
     const artifacts: string[] = [];
 
     for (const artifact in build.artifacts) {
-        artifacts.push(artifact);
+        artifacts.push(artifact.replace('/target', ''));
     }
 
     return artifacts;
